ab941530c069fbcf74c0be491fecc9a2f3237ec9
Olekciy <0lekciy4@gmail.com>
2018-04-11 09:47:54 +0300

    Realized search for friends online in vk.com

--- README.md
+++ README.md
@@ -1,10 +1,38 @@
 # Watcher of Friends Online
+This script prints in the terminal a list of your friends online in vk.
+Before the first start, you must get **"APP_ID"** . To get **"APP_ID"**,
+you need to register your
+application at **https://vk.com/dev**
 
-[TODO. There will be project description]
+# Example run
+Open the terminal, go to the directory where the file is located
+and run **"python vk_friends_online.py"**, if python 3 as not default
+try **"python3 vk_friends_online.py"**
+
+
+```bash
+
+$ python vk_friends_online.py
+please enter APP_ID, or change variable APP_ID
+in 'vk_friends_online.py': <your app_id>
+Enter please your vk login
+<your vk_login>
+Enter please your vk password
+<your vk_password>
+Your friends online
+<First_name1> <Last_name1>
+<First_name2> <Last_name2>
+
+. . .
+
+
+```
 
 # How to Install
 
-Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+Python 3 should be already installed.
+Then use **pip** (or pip3 if there is a conflict with old Python 2 setup)
+to install dependencies:
 
 ```bash
 pip install -r requirements.txt # alternatively try pip3
--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,32 +1,57 @@
+from getpass import getpass
+
 import vk
 
-
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = None
+if not APP_ID:
+    APP_ID = int(input('please enter APP_ID, or change variable APP_ID'
+                       '\nin \'vk_friends_online.py\': '))
 
 
 def get_user_login():
-    pass
+    user_login = input('Enter please your vk login\n')
+    return user_login
 
 
 def get_user_password():
-    pass
+    user_password = getpass('Enter please your vk password\n')
+    return user_password
 
 
 def get_online_friends(login, password):
-    session = vk.AuthSession(
-        app_id=APP_ID,
-        user_login=login,
-        user_password=password,
-    )
-    api = vk.API(session)
-    # например, api.friends.get()
+    try:
+        session = vk.AuthSession(
+            app_id=APP_ID,
+            user_login=login,
+            user_password=password,
+            scope='friends',
+        )
+        api = vk.API(session, v='5.73', lang='ru', timeout=10)
+        friends_online_ids = api.friends.getOnline()
+        friends_online_dict = (api.users.get(user_ids=friends_online_ids))
+        return friends_online_dict
+    except vk.exceptions.VkAuthError:
+        return None
 
 
 def output_friends_to_console(friends_online):
-    pass
+    if friends_online:
+        print('Your friends online')
+        for friend in friends_online:
+            print(friend['first_name'], friend['last_name'])
+    else:
+        print('Incorrect password or login')
 
-if __name__ == '__main__':
+
+def main():
     login = get_user_login()
     password = get_user_password()
-    friends_online = get_online_friends(login, password)
-    output_friends_to_console(friends_online)
+    if login and password:
+        friends_online = get_online_friends(login, password)
+        output_friends_to_console(friends_online)
+    else:
+        print('Login and password must be non-empty')
+
+
+if __name__ == '__main__':
+    main()
